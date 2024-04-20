42daf5c504290b9e061eb77d48ef46fcd072936b
Jonathan Santos <jonathan.tecsantos@gmail.com>
2024-04-19 23:35:06 -0300

    Processo de importacao de Grupos e Alunos

---
+++ Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/PlataformaempreendedorismoApplication.java
@@ -0,0 +1,13 @@
+package com.plataforma.empreendedorismo.plataformaempreendedorismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaempreendedorismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaempreendedorismoApplication.class, args);
+	}
+
+}
---
+++ Backend/plataformaempreendedorismo/src/test/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/PlataformaempreendedorismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataforma.empreendedorismo.plataformaempreendedorismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaempreendedorismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

cbf90e2df721ccaa37171a84c846b93952833941
winnicius-moura <winnicius.moura@gmail.com>
2024-08-05 19:33:21 -0300

    feat: switch colors theme

--- Frontend/plataformaempreendedorismo/src/admin/components/common/defaultPage.tsx
+++ Frontend/plataformaempreendedorismo/src/admin/components/common/defaultPage.tsx
@@ -21,7 +21,7 @@ const AdminAppBar = () => {
   const userGlobalState = useSelector((state: RootState) => state.userInfo.data)
 
   return (
-    <div className="flex bg-[#152259] text-white justify-between lg:p-4 p-1 px-4 fixed top-0 left-0 right-0 z-10">
+    <div className="flex  text-[#3C14A4] justify-between lg:p-4 p-1 px-4 fixed top-0 left-0 right-0 z-10">
       <div className="block lg:hidden">
         <DrawerComponent />
       </div>
@@ -48,7 +48,7 @@ const LeftMenu = () => {
   const navigate = useNavigate()
 
   return (
-    <div className=" text-[#cecece] h-full">
+    <div className=" text-[#3C14A4] h-full">
       <div className="w-full p-4">
         {/* <img
           src={backgroundImageUrl}
@@ -57,7 +57,7 @@ const LeftMenu = () => {
         /> */}
         <h2
           onClick={() => navigate(RoutesNames.home)}
-          className="cursor-pointer font-bold text-center text-white bg-gradient-to-r from-blue-500 to-blue-900 p-2 rounded-lg shadow-lg transform hover:scale-105 transition-transform duration-300"
+          className="cursor-pointer font-bold text-center text-white bg-gradient-to-r from-indigo-500 to-indigo-900 p-2 rounded-lg shadow-lg transform hover:scale-105 transition-transform duration-300"
         >
           Plataforma Empreendedorismo
         </h2>
@@ -73,11 +73,11 @@ export const AdminDefaultPage = ({ mainContent }: AdminPage) => {
     <div className="flex h-screen flex-col">
       <AdminAppBar />
       <div className="flex flex-1 overflow-hidden">
-        <div className="lg:block hidden w-64 h-full shadow-lg z-10 overflow-y-auto bg-[#152259]">
+        <div className="lg:block hidden w-64 h-full shadow-lg z-10 overflow-y-auto">
           <LeftMenu />
         </div>
         <main className="overflow-x-hidden overflow-y-auto p-4 w-full mt-14 rounded-lg">
-          <div className="p-4 shadow-md border mb-4 rounded-3xl bg-white">
+          <div className="p-4 shadow-md border mb-4 rounded-3xl">
             <BreadcrumbComponent />
             {mainContent}
           </div>

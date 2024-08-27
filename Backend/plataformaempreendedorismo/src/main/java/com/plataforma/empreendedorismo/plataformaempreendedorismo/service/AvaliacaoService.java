04d2f842eb25dded0b426faeb6ae5e9e4a362376
Jonathan Santos <jonathan.tecsantos@gmail.com>
2024-08-26 21:48:38 -0300

    fix manutenção Equipe

--- Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/service/AvaliacaoService.java
+++ Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/service/AvaliacaoService.java
@@ -1,14 +1,12 @@
 package com.plataforma.empreendedorismo.plataformaempreendedorismo.service;
 
 import com.plataforma.empreendedorismo.plataformaempreendedorismo.model.CriterioAvaliacao;
-import com.plataforma.empreendedorismo.plataformaempreendedorismo.model.FormatoAvaliacao;
 import com.plataforma.empreendedorismo.plataformaempreendedorismo.record.avaliacao.FormatoAvaliacaoRecord;
 import com.plataforma.empreendedorismo.plataformaempreendedorismo.repository.CriterioAvaliacaoRepository;
 import com.plataforma.empreendedorismo.plataformaempreendedorismo.repository.FormatoAvaliacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 

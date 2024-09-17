e3bee8c1675d3184409fc6596739332b33b4ef54
Jonathan Santos <jonathan.tecsantos@gmail.com>
2024-09-16 21:44:33 -0300

    Enpoint para buscar informações da avaliação

--- Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/record/avaliacao/AvaliacaoRecord.java
+++ Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/record/avaliacao/AvaliacaoRecord.java
@@ -1,6 +1,14 @@
 package com.plataforma.empreendedorismo.plataformaempreendedorismo.record.avaliacao;
 
+import com.plataforma.empreendedorismo.plataformaempreendedorismo.model.Avaliacao;
+
 public record AvaliacaoRecord(
-        CriterioRecord criterioRecord
+        Long idCriterioAvaliacao,
+        Long idSubcriterioAvaliacao,
+        Double nota
 ) {
+    public AvaliacaoRecord(Avaliacao avaliacao){
+        this(avaliacao.getIdCriterioAvaliacao(),avaliacao.getIdSubcriterioAvaliacao(), avaliacao.getNota());
+    }
+
 }
--- Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/repository/AvaliacaoRepository.java
+++ Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/repository/AvaliacaoRepository.java
@@ -2,7 +2,22 @@ package com.plataforma.empreendedorismo.plataformaempreendedorismo.repository;
 
 import com.plataforma.empreendedorismo.plataformaempreendedorismo.model.Avaliacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
     Avaliacao findByIdEquipeAndIdCriterioAvaliacaoAndIdSubcriterioAvaliacaoAndIdAvaliador(Long idEquipe, Long criterioAvaliacaoId, Long subCriterioAvaliacaoId, Long avaliadorId);
+
+    @Query(value = "SELECT av.* FROM avaliacao av " +
+            "JOIN criterio_avaliacao ca ON av.id_criterio_avaliacao = ca.id " +
+            "WHERE ca.id_formato_avaliacao = :idFormatoAvaliacao " +
+            "AND av.id_avaliador = :idAvaliador",
+            nativeQuery = true)
+    List<Avaliacao> findAvaliacoesByAvaliadorAndFormato(
+            @Param("idFormatoAvaliacao") Long idFormatoAvaliacao,
+            @Param("idAvaliador") Long idAvaliador);
+
+
 }

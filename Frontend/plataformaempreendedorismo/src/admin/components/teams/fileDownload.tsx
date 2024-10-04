05fcf2e7410427ab8a4d9a8d1dc44ed36325d454
winnicius-moura <winnicius.moura@gmail.com>
2024-10-04 18:34:19 -0300

    feat: fileDownload

--- Frontend/plataformaempreendedorismo/src/admin/components/teams/fileDownload.tsx
+++ Frontend/plataformaempreendedorismo/src/admin/components/teams/fileDownload.tsx
@@ -7,31 +7,35 @@ interface FileDownloadProps {
   type: AnexoTypeDescription
 }
 
-const downloadFile = (caminho: string, nomeAnexo: string) => {
-  // Simulando o caminho local, deve ser adaptado conforme o servidor
-  const url = caminho.replace(/\\/g, '/'); // Para lidar com o formato Windows de caminho
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = nomeAnexo;
-  a.click();
-};
+const downloadFile = (nomeAnexo: string) => {
+  const url = `${import.meta.env.VITE_API_URL}/download/${nomeAnexo}`
+  const a = document.createElement('a')
+  a.href = url
+  a.download = nomeAnexo
+  a.click()
+}
 
 export const FileDownload = ({ anexos, type }: FileDownloadProps) => {
-  const anexosFiltrados = anexos?.filter(anexo => anexo.tipoAnexoPrototipo.descricao === type);
+  const anexosFiltrados = anexos?.filter(anexo => anexo.tipoAnexoPrototipo.descricao === type)
   return (
     <div>
       {anexosFiltrados.length > 0 && (
-        <div className="mt-4 text-[#3C14A4] ">
-          {anexosFiltrados.map(anexo => (
-            <div key={anexo.id}
-              className="flex items-center border border-gray-300 p-2 rounded-md mb-2 shadow-sm">
-              <ImageIcon className="text-blue-500 mr-2" />
-              <span className="flex-grow">{anexo.nomeAnexo}</span>
-              <DownloadIcon className="text-green-500 cursor-pointer"
-                onClick={() => downloadFile(anexo.caminhoAnexo, anexo.nomeAnexo)}
-              />
-            </div>
-          ))}
+        <div className="mt-4 text-[#3C14A4]">
+          <div className="flex flex-wrap space-x-2">
+            {anexosFiltrados.map(anexo => (
+              <div key={anexo.id}
+                className="flex items-center border border-gray-300 p-2 rounded-md shadow-sm flex-shrink-0 flex-grow w-32 h-12">
+                <ImageIcon className="text-blue-500 mr-2" />
+                <span className="flex-grow overflow-hidden whitespace-nowrap text-ellipsis">
+                  {anexo.nomeAnexo}
+                </span>
+                <DownloadIcon
+                  className="text-green-500 cursor-pointer"
+                  onClick={() => downloadFile(anexo.nomeAnexo)}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </div>

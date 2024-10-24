4d58f72657e7eddf8b3f6f61a57e6c76340cbe39
Jonathan Santos <jonathan.tecsantos@gmail.com>
2024-10-24 18:08:49 -0300

    Feature eventos - fix swagger eventos

--- Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/controller/EventoController.java
+++ Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/controller/EventoController.java
@@ -6,16 +6,19 @@ import com.plataforma.empreendedorismo.plataformaempreendedorismo.service.Evento
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/eventos")
+@Tag(name="Evento")
+@SecurityRequirement(name = "bearerToken")
 public class EventoController {
 
     @Autowired

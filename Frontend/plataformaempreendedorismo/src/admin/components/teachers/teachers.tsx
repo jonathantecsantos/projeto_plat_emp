b52961ccc855b4c4b5839792f53b30f41da6f208
winnicius-moura <winnicius.moura@gmail.com>
2024-11-21 22:18:34 -0300

    feat: teacher delet and reset pwd

--- Frontend/plataformaempreendedorismo/src/admin/components/teachers/teachers.tsx
+++ Frontend/plataformaempreendedorismo/src/admin/components/teachers/teachers.tsx
@@ -1,41 +1,48 @@
+import CloseIcon from '@mui/icons-material/Close'
 import CloudUploadIcon from '@mui/icons-material/CloudUpload'
-import ClearIcon from '@mui/icons-material/Clear'
-import { Button, CircularProgress, Dialog, DialogActions, DialogContent, IconButton } from '@mui/material'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import { LoadingButton } from '@mui/lab'
+import { Button, CircularProgress, Dialog, DialogActions, DialogContent, IconButton, Menu, MenuItem } from '@mui/material'
 import { useSnackbar } from "notistack"
 import { useEffect, useMemo, useRef, useState } from "react"
 import { useNavigate, useSearchParams } from "react-router-dom"
-import { useDeleteTeacherMutation, useGetTeachersQuery } from "../../../api/studentApi"
+import { useDeleteTeacherMutation, useGetTeachersQuery, usePasswordResetMutation } from "../../../api/studentApi"
 import { RoutesNames } from "../../../globals"
 import { TeachersResponse } from "../../../model/teacher"
+import { Roles } from '../../../utils/types'
 import { AdminHeader } from "../common/adminHeader"
 import { TableComponent } from '../table'
 import { TableComponentClickRowProps, TableComponentSetCurrPageProps } from "../table/common"
 
 export const TeachersComponent = () => {
   const { data: teachers, isLoading, error, refetch } = useGetTeachersQuery()
+  const [deleteTeacher, { isLoading: teacherDelete }] = useDeleteTeacherMutation()
+  const [passordReset, { isLoading: resetPassword }] = usePasswordResetMutation()
   const [searchParams, setSearchParams] = useSearchParams()
   const searchTerm = searchParams.get('search') || ''
 
-  const [open, setOpen] = useState(false)
   const [selectedTeacher, setSelectedTeacher] = useState<TeachersResponse>()
-  const [deleteStudent] = useDeleteTeacherMutation()
-
+  const [actionType, setActionType] = useState<'delete' | 'resetPassword' | null>(null)
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const [openDialog, setOpenDialog] = useState(false)
 
   const navigate = useNavigate()
   const tableComponentSetCurrPageRef = useRef<TableComponentSetCurrPageProps>(() => { })
   const tableComponentSetCurrPage = tableComponentSetCurrPageRef.current
-  if (tableComponentSetCurrPage) tableComponentSetCurrPage({ page: 0 })
-  const { enqueueSnackbar } = useSnackbar()
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar()
 
   useEffect(() => {
-    if (teachers) {
-      if (teachers?.length <= 0) {
-        console.log('teachers refetch')
-        refetch()
-      }
+    if (teachers?.length === 0) {
+      console.log('teachers refetch')
+      refetch()
     }
   }, [])
 
+  useEffect(() => {
+    if (tableComponentSetCurrPage) {
+      tableComponentSetCurrPage({ page: 0 });
+    }
+  }, [teachers])
 
   const filteredStudents = useMemo(() => {
     if (!teachers) return []
@@ -47,23 +54,67 @@ export const TeachersComponent = () => {
     setSearchParams({ search: query })
   }
 
-  const handleClickOpen = ({ teacher }: { teacher: TeachersResponse }) => {
-    setSelectedTeacher(teacher)
-    setOpen(true)
+  const handleCloseMenu = () => {
+    setAnchorEl(null)
   }
 
-  const handleDeleteStudent = async () => {
-    if (selectedTeacher) {
+  const handleOpenMenu = (event: React.MouseEvent<HTMLElement>, teacher: TeachersResponse) => {
+    setSelectedTeacher(teacher)
+    setAnchorEl(event.currentTarget)
+  }
+
+
+  const handleOpenDialog = (type: 'delete' | 'resetPassword', teacher: TeachersResponse) => {
+    setActionType(type)
+    setSelectedTeacher(teacher)
+    setOpenDialog(true)
+    handleCloseMenu()
+  }
+
+  const handleCloseDialog = () => {
+    setActionType(null)
+    setSelectedTeacher(undefined)
+    setOpenDialog(false)
+  }
+
+
+  const handleConfirmAction = async () => {
+    if (actionType === 'delete' && selectedTeacher) {
       try {
-        await deleteStudent(selectedTeacher.id)
-        enqueueSnackbar(`Professor: ${selectedTeacher.nome}, excluído com sucesso!`, { variant: 'success' })
+        await deleteTeacher(selectedTeacher.id)
+        enqueueSnackbar(`Professor(a): ${selectedTeacher.nome} excluído(a) com sucesso!`, { variant: 'success' })
         refetch()
-        setOpen(false)
       } catch (error) {
         enqueueSnackbar('Erro ao excluir, consulte um administrador.', { variant: 'error' })
       }
+    } else if (actionType === 'resetPassword' && selectedTeacher) {
+      try {
+        const response = await passordReset({
+          idObjeto: selectedTeacher.id,
+          emailUsuario: selectedTeacher.email,
+          role: Roles.Professor,
+        })
+        enqueueSnackbar(`Senha do Professor(a): ${selectedTeacher.nome} foi resetada com sucesso!`, { variant: 'success' })
+        enqueueSnackbar(
+          <div>
+            <p><strong>Login:</strong> {response?.data?.login}</p>
+            <p><strong>Nova senha:</strong> {response?.data?.senha}</p>
+          </div>,
+          {
+            variant: 'info',
+            persist: true,
+            action: (key) => (
+              <CloseIcon className='hover:cursor-pointer hover:text-red-500' onClick={() => closeSnackbar(key)} color="primary" />
+            ),
+          }
+        )
+      } catch (error) {
+        enqueueSnackbar('Erro ao resetar senha, consulte um administrador.', { variant: 'error' })
+      }
     }
+    handleCloseDialog()
   }
+
 
   if (isLoading) return <div className='text-center'><CircularProgress /></div>
   if (error) return <p className="text-center">Error loading teachers.</p>
@@ -112,40 +163,74 @@ export const TeachersComponent = () => {
                 <td className="px-4 py-2 capitalize">{teacher.equipeRecord.nome.toLowerCase()}</td>
                 <td className="">
                   <IconButton
+                    className='hover:text-white no-row-click'
                     aria-controls="long-menu"
                     aria-haspopup="true"
                     onClick={(event) => {
                       event.stopPropagation()
-                      handleClickOpen({ teacher: teacher })
+                      handleOpenMenu(event, teacher)
                     }}
                   >
-                    <ClearIcon style={{
-                      width: 15,
-                      height: 15,
-                    }} className='hover:text-red-500' />
+                    <MoreVertIcon />
                   </IconButton>
+                  <Menu
+                    className='no-row-click'
+                    variant='selectedMenu'
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={handleCloseMenu}>
+                    <MenuItem className='no-row-click'
+                      onClick={() => handleOpenDialog('delete', selectedTeacher!)}>
+                      Excluir
+                    </MenuItem>
+
+                    <MenuItem className='no-row-click'
+                      onClick={() => handleOpenDialog('resetPassword', selectedTeacher!)}>
+                      Resetar Senha
+                    </MenuItem>
+                  </Menu>
                 </td>
               </>
             )}
             onClickRow={(teacher: TableComponentClickRowProps<TeachersResponse>) => {
+              const target = teacher.target as HTMLElement
+              if (target.closest('.no-row-click')) {
+                return
+              }
               navigate(RoutesNames.teacher.replace(':id', teacher.item?.id.toString()))
             }}
           />
         </div>
       </div>
-      <Dialog open={open} onClose={() => setOpen(false)}>
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
         <DialogContent>
-          <span>
-            Deseja realmente excluir o professor(a): {selectedTeacher?.nome.toLowerCase()}?
-          </span>
+          {actionType === 'delete' && (
+            <span>Deseja realmente excluir o aluno(a): {selectedTeacher?.nome.toLowerCase()}?</span>
+          )}
+          {actionType === 'resetPassword' && (
+            <span>Deseja realmente resetar a senha do aluno(a): {selectedTeacher?.nome.toLowerCase()}?</span>
+          )}
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => setOpen(false)} style={{ textTransform: 'none', color: 'gray' }}>
+          <Button onClick={handleCloseDialog} style={{ textTransform: 'none', color: 'gray' }}>
             Cancelar
           </Button>
-          <Button onClick={handleDeleteStudent} style={{ textTransform: 'none', color: 'red' }}>
-            Excluir
-          </Button>
+          <LoadingButton
+            style={{
+              textTransform: 'none',
+              color: actionType === 'delete' ? 'red' : 'blue',
+              backgroundColor: 'transparent',
+              borderRadius: '0.375rem',
+              boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+              transition: 'background-color 0.3s',
+            }}
+            variant='contained'
+            loading={resetPassword || teacherDelete}
+            disabled={resetPassword || teacherDelete}
+            onClick={handleConfirmAction}
+          >
+            {actionType === 'delete' ? 'Excluir' : 'Resetar Senha'}
+          </LoadingButton>
         </DialogActions>
       </Dialog>
     </div>

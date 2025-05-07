c7564b32ac1b46461619c8831ac7d0f0653ca247
winnicius-moura <winnicius.moura@gmail.com>
2025-05-07 00:15:37 -0300

    style: login page & footer add

--- Frontend/plataformaempreendedorismo/src/components/common/footer.tsx
+++ Frontend/plataformaempreendedorismo/src/components/common/footer.tsx
@@ -1,13 +1,44 @@
-interface FooterProps {
-  username: string
-}
 
-export const Footer = ({ username }: FooterProps) => {
+export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="text-gray-400 flex justify-start items-center py-2 w-full text-[10px] print:hidden">
-      <div className="text-center px-2 ">
-        {username} Online Version: {import.meta.env.VITE_APP_VERSION}
+    <footer className="bg-gray-800 text-white py-8 px-4 mt-auto">
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="md:text-start text-center  mb-4 md:mb-0">
+            <h2 className="text-xl font-bold">JW Softwares</h2>
+            <p className="text-gray-400 mt-2">Soluções digitais, impacto real.</p>
+            {/* <p className="text-gray-400 mt-2">Potencializando o ensino com inovação sob medida.</p> */}
+          </div>
+
+          <div className="flex space-x-6">
+            <a href="/termos" className="hover:text-gray-300 transition-colors">Termos de Serviço</a>
+            <a href="/privacidade" className="hover:text-gray-300 transition-colors">Política de Privacidade</a>
+            <a href="/contato" className="hover:text-gray-300 transition-colors">Contato</a>
+          </div>
+        </div>
+
+        <div className="border-t border-gray-700 mt-6 pt-6 text-center text-gray-400">
+          <p>© {currentYear} JW Softwares. Todos os direitos reservados.</p>
+          <p className="mt-2 text-sm">Site seguro</p>
+        </div>
       </div>
     </footer>
   )
 }
+
+export const Footer2 = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-6 px-4 mt-auto">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center">
+        <p className="text-sm">&copy; {new Date().getFullYear()} JW Softwares. Todos os direitos reservados.</p>
+        <div className="flex space-x-4 mt-2 md:mt-0">
+          <a href="/politica-de-privacidade" className="text-sm hover:underline">Privacidade</a>
+          <a href="/termos-de-uso" className="text-sm hover:underline">Termos</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
--- Frontend/plataformaempreendedorismo/src/site/components/login.tsx
+++ Frontend/plataformaempreendedorismo/src/site/components/login.tsx
@@ -5,11 +5,13 @@ import { useSnackbar } from "notistack"
 import { ChangeEvent, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useGetEventValidateByIdQuery } from "../../api/studentApi"
+import lampLogo from '../../assets/lamplogo.png'
+import { Footer, Footer2 } from "../../components/common/footer"
 import { RoutesNames } from "../../globals"
 import { EventsTypes } from "../../model/config"
 import { UserApiService } from "../../services/login"
 import { Login, LoginTokenJWT, Roles } from "../../utils/types"
-import { Footer } from "../../components/common/footer"
+
 
 
 export const LoginComponent = () => {
@@ -58,51 +60,55 @@ export const LoginComponent = () => {
   }
 
   return (
-    <div className="flex justify-center w-full h-dvh">
-      <div className="hidden lg:block w-2/3 h-dvh box-border rounded-md lg:mr-24">
-        <img
-          src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          className="w-dvw h-dvh object-cover"
-        />
-      </div>
-      <form action="">
-        <div className={`p-10 rounded-md h-fit ${isValid && isValid ? 'xl:mt-12 md:mt-4' : 'mt-20'} sm:w-96 shadow-xl w-full lg:m-28 lg:ml-12 border-t-2 `}>
-          <h1 className="font-bold text-xl w-full mb-10 cursor-pointer"
-            onClick={() => navigate(RoutesNames.home)}>Plataforma Empreendedorismo</h1>
-
-          <h2 className="font-medium mb-4 w-fit">Acesse sua conta</h2>
-          <div className="flex flex-col gap-8 text-start">
-            <div className="flex flex-col text-sm text-[#888]">
-              <label htmlFor="">Usuário</label>
-              <input type="text" placeholder="Seu usuário" className="rounded-md p-2 shadow-md "
-                onChange={handleUsernameChange}
-              />
-            </div>
-            <div className="flex flex-col text-sm text-[#888]">
-              <label htmlFor="">Senha</label>
-              <input type="password" placeholder="Sua senha" className="rounded-md p-2 shadow-md "
-                onChange={handlePasswordChange}
-              />
-            </div>
-            <LoadingButton loading={isLoading} disabled={isLoading}
-              className="bg-ring-custom normal-case mt-8 shadow-md hover:bg-[#8668FFCC]"
-              variant="contained" onClick={handleLogin} >
-              <span>Entrar</span>
-            </LoadingButton>
-          </div>
-          {registerLoading ? <div className='text-center my-2'><LinearProgress /></div> :
-            isValid && isValid ? <div className="flex flex-col gap-2 mt-8 text-center">
-              <div className="text-sm text-[#888]">Inscrições abertas!</div>
-              <div className="text-sm text-[#888]">Acesse o formulário e faça sua inscrição!</div>
-              <LoadingButton
-                className="border-l-indigo-950 mx-auto normal-case w-full shadow-md hover:scale-105"
-                variant="text" onClick={() => navigate(RoutesNames.register)} >
-                <span>Inscrição DLEI</span>
-              </LoadingButton>
-            </div> : null}
+    <div>
+      <div className="flex justify-center w-full h-dvh">
+        <div className="hidden lg:block w-2/3 h-dvh box-border rounded-md lg:mr-24">
+          <img
+            src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="w-dvw h-dvh object-cover"
+          />
         </div>
-        <Footer username=""/>
-      </form>
+        <form action="">
+          <div className={`p-6 rounded-md h-fit ${isValid && isValid ? 'xl:mt-12 md:mt-4' : 'mt-20'} sm:w-96 shadow-xl w-full lg:m-28 lg:ml-12 border-t-2 `}>
+            <img src={lampLogo} alt="Trophy" className="cursor-pointer mx-auto transform hover:scale-125 transition-transform duration-300 w-20 h-20 hover:rotate-12" />
+            <h1 className="font-bold text-xl w-full my-6 cursor-pointer"
+              onClick={() => navigate(RoutesNames.home)}>Plataforma Empreendedorismo</h1>
+
+            <h2 className="font-medium mb-4 w-fit">Acesse sua conta</h2>
+            <div className="flex flex-col gap-4 text-start">
+              <div className="flex flex-col text-sm text-[#888]">
+                <label htmlFor="">Usuário</label>
+                <input type="text" placeholder="Seu usuário" className="rounded-md p-2 shadow-md "
+                  onChange={handleUsernameChange}
+                />
+              </div>
+              <div className="flex flex-col text-sm text-[#888]">
+                <label htmlFor="">Senha</label>
+                <input type="password" placeholder="Sua senha" className="rounded-md p-2 shadow-md "
+                  onChange={handlePasswordChange}
+                />
+              </div>
+              <LoadingButton loading={isLoading} disabled={isLoading}
+                className="bg-ring-custom normal-case my-4 shadow-md hover:bg-[#8668FFCC]"
+                variant="contained" onClick={handleLogin} >
+                <span>Entrar</span>
+              </LoadingButton>
+            </div>
+            {registerLoading ? <div className='text-center my-2'><LinearProgress /></div> :
+              isValid && isValid ? <div className="flex flex-col gap-2 mt-8 text-center">
+                <div className="text-sm text-[#888]">Inscrições abertas!</div>
+                <div className="text-sm text-[#888]">Acesse o formulário e faça sua inscrição!</div>
+                <LoadingButton
+                  className="border-l-indigo-950 mx-auto normal-case w-full shadow-md hover:scale-105"
+                  variant="text" onClick={() => navigate(RoutesNames.register)} >
+                  <span>Inscrição DLEI</span>
+                </LoadingButton>
+              </div> : null}
+          </div>
+        </form>
+      </div>
+      <Footer />
     </div>
+
   )
 }

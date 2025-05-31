c0020bc04da4e2c2a7cc5b9d44942bbf3c79bade
Jonathan Santos <jonathan.tecsantos@gmail.com>
2025-05-31 11:32:16 -0300

    refactor

--- Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/infra/security/SecurityConfigurations.java
+++ Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/infra/security/SecurityConfigurations.java
@@ -1,5 +1,4 @@
 package com.plataforma.empreendedorismo.plataformaempreendedorismo.infra.security;
-
 import com.plataforma.empreendedorismo.plataformaempreendedorismo.infra.config.CustomAccessDeniedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
--- Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/infra/security/SecurityFilter.java
+++ Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/infra/security/SecurityFilter.java
@@ -18,27 +18,22 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private TokenService tokenService;
-
     @Autowired
     private UsuarioRepository usuarioRepository;
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        //Recupera token da requisicao
         var tokenJWT = recuperarToken(request);
 
         if(tokenJWT != null) {
-            //Verifica se o token he valido
             var subject = tokenService.getSubject(tokenJWT);
 
-            //Forca autenticacao do usuario na API
             var usuario = usuarioRepository.findByLogin(subject);
             var authentication = new UsernamePasswordAuthenticationToken(usuario,null,usuario.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
         }
-        //Segue o fluxo do filtro
+
         filterChain.doFilter(request,response);
     }
 

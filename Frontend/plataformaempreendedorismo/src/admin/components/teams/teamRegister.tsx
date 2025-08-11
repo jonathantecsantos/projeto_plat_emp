baeef51e1fb27827a0b81409f32182303849c813
winnicius-moura <winnicius.moura@gmail.com>
2025-08-11 19:01:16 -0300

    refactor: adjust print styles and improve spacing in TeamRegisterPrintComponent

--- Frontend/plataformaempreendedorismo/src/admin/components/teams/teamRegister.tsx
+++ Frontend/plataformaempreendedorismo/src/admin/components/teams/teamRegister.tsx
@@ -42,21 +42,21 @@ export const TeamRegisterPrintComponent = ({ id }: TeamRegisterPrintComponentPro
 
 
   return (
-    <div ref={printRef} className="w-full max-w-6xl mx-auto bg-white p-2 print:p-2 print:text-[12px]">
-      <img src={lampLogo} alt="Trophy" className="w-16 h-16 absolute inset-8" />
-      <div className="print:max-w-md print:mx-auto print:mt-2">
-        <h1 className="text-start text-xl font-bold text-[#2f5597]">DLEI 2025</h1>
+    <div ref={printRef} className="w-full max-w-7xl mx-auto bg-white p-2 print:p-2 print:text-[12px]">
+      <img src={lampLogo} alt="Trophy" className="w-16 h-16 absolute inset-6" />
+      <div className="print:max-w-md print:mx-auto">
+        <h1 className="text-start text-base font-bold text-[#2f5597]">DLEI 2025</h1>
         <div className="w-full"></div>
-        <h2 className="text-start text-nowrap text-lg font-bold mb-2 text-[#2f5597]">9º Desafio Lourdinas de Empreendedorismo e Inovação</h2>
+        <h2 className="text-start text-nowrap text-base font-bold mb-2 text-[#2f5597]">9º Desafio Lourdinas de Empreendedorismo e Inovação</h2>
       </div>
-      <h2 className="text-center text-lg font-semibold mb-2">FICHA DE INSCRIÇÃO / RECIBO</h2>
+      <h2 className="text-center text-base font-semibold mb-2">FICHA DE INSCRIÇÃO / RECIBO</h2>
       <table className="w-full text-sm bg-[#fc56f4] border border-b-0 border-gray-700">
         <tbody>
           <tr>
-            <td className="font-semibold border border-b-0 border-gray-700 align-top w-[130px] p-1">
+            <td className="font-semibold border border-b-0 border-gray-700 align-top w-[130px] p-0.5">
               Nome do Time
             </td>
-            <td className="p-1 font-bold border border-b-0 border-gray-700">
+            <td className="p-0.5 font-bold border border-b-0 border-gray-700">
               {teamRegister?.nomeEquipe}
             </td>
           </tr>
@@ -66,56 +66,56 @@ export const TeamRegisterPrintComponent = ({ id }: TeamRegisterPrintComponentPro
       <table className="w-full border border-b-0 border-gray-700 print:border-collapse text-sm">
         <thead className="bg-gray-200">
           <tr>
-            <th className="border border-gray-700 p-1">Nº Aluno</th>
-            <th className="border border-gray-700 p-1">Nome do Aluno</th>
-            <th className="border border-gray-700 p-1">E-mail</th>
-            <th className="border border-gray-700 p-1">CPF</th>
-            <th className="border border-gray-700 p-1">Data Nasc.</th>
-            <th className="border border-gray-700 p-1">Nº Camisa</th>
+            <th className="border border-gray-700 p-0.5">Nº Aluno</th>
+            <th className="border border-gray-700 p-0.5">Nome do Aluno</th>
+            <th className="border border-gray-700 p-0.5">E-mail</th>
+            <th className="border border-gray-700 p-0.5">CPF</th>
+            <th className="border border-gray-700 p-0.5">Data Nasc.</th>
+            <th className="border border-gray-700 p-0.5">Nº Camisa</th>
           </tr>
         </thead>
         <tbody>
           {leader && (
             <tr>
-              <td className="border border-gray-700 p-1 text-center font-semibold max-w-36">01 - LÍDER</td>
-              <td className="border border-gray-700 p-1">{leader.nome}</td>
-              <td className="border border-gray-700 p-1 text-wrap">{leader.email}</td>
-              <td className="border border-gray-700 p-1 text-nowrap">{maskCPF(leader.cpf)}</td>
-              <td className="border border-gray-700 p-1 text-center">{leader.dataNascimento && formatDate(leader.dataNascimento.toString())}</td>
-              <td className="border border-gray-700 p-1 text-center">{leader.tamanhoCamisa}</td>
+              <td className="border border-gray-700 p-0.5 text-center font-semibold max-w-36">01 - LÍDER</td>
+              <td className="border border-gray-700 p-0.5">{leader.nome}</td>
+              <td className="border border-gray-700 p-0.5 text-wrap">{leader.email}</td>
+              <td className="border border-gray-700 p-0.5 text-nowrap">{maskCPF(leader.cpf)}</td>
+              <td className="border border-gray-700 p-0.5 text-center">{leader.dataNascimento && formatDate(leader.dataNascimento.toString())}</td>
+              <td className="border border-gray-700 p-0.5 text-center">{leader.tamanhoCamisa}</td>
             </tr>
           )}
 
           {viceLeader && (
             <tr>
-              <td className="border border-gray-700 p-1 text-center font-semibold">02 - VICE-LÍDER</td>
-              <td className="border border-gray-700 p-1">{viceLeader.nome}</td>
-              <td className="border border-gray-700 p-1">{viceLeader.email}</td>
-              <td className="border border-gray-700 p-1">{maskCPF(viceLeader.cpf)}</td>
-              <td className="border border-gray-700 p-1 text-center">{viceLeader.dataNascimento && formatDate(viceLeader.dataNascimento.toString())}</td>
-              <td className="border border-gray-700 p-1 text-center">{viceLeader.tamanhoCamisa}</td>
+              <td className="border border-gray-700 p-0.5 text-center font-semibold">02 - VICE-LÍDER</td>
+              <td className="border border-gray-700 p-0.5">{viceLeader.nome}</td>
+              <td className="border border-gray-700 p-0.5">{viceLeader.email}</td>
+              <td className="border border-gray-700 p-0.5">{maskCPF(viceLeader.cpf)}</td>
+              <td className="border border-gray-700 p-0.5 text-center">{viceLeader.dataNascimento && formatDate(viceLeader.dataNascimento.toString())}</td>
+              <td className="border border-gray-700 p-0.5 text-center">{viceLeader.tamanhoCamisa}</td>
             </tr>
           )}
 
           {members?.map((student, index) => (
             <tr key={student.id || index}>
-              <td className="border border-gray-700 p-1 text-center font-semibold">{String(index + 3).padStart(2, "0")} - INTEGRANTE</td>
-              <td className="border border-gray-700 p-1">{student.nome}</td>
-              <td className="border border-gray-700 p-1">{student.email}</td>
-              <td className="border border-gray-700 p-1">{maskCPF(student.cpf)}</td>
-              <td className="border border-gray-700 p-1 text-center">{student.dataNascimento && formatDate(student.dataNascimento.toString())}</td>
-              <td className="border border-gray-700 p-1 text-center">{student.tamanhoCamisa}</td>
+              <td className="border border-gray-700 p-0.5 text-center font-semibold">{String(index + 3).padStart(2, "0")} - INTEGRANTE</td>
+              <td className="border border-gray-700 p-0.5">{student.nome}</td>
+              <td className="border border-gray-700 p-0.5">{student.email}</td>
+              <td className="border border-gray-700 p-0.5">{maskCPF(student.cpf)}</td>
+              <td className="border border-gray-700 p-0.5 text-center">{student.dataNascimento && formatDate(student.dataNascimento.toString())}</td>
+              <td className="border border-gray-700 p-0.5 text-center">{student.tamanhoCamisa}</td>
             </tr>
           ))}
 
           {teamRegister?.professores.map((professor, index) => (
             <tr key={professor.id || `prof-${index}`}>
-              <td className="border border-gray-700 p-1 text-center font-semibold w-[130px]">PROFESSOR ORIENTADOR</td>
-              <td className="border border-gray-700 p-1">{professor.nome}</td>
-              <td className="border border-gray-700 p-1">{professor.email}</td>
-              <td className="border border-gray-700 p-1">{maskCPF(professor.cpf)}</td>
-              <td className="border border-gray-700 p-1 text-center">{professor.dataNascimento && formatDate(professor.dataNascimento.toString())}</td>
-              <td className="border border-gray-700 p-1 text-center">{professor.tamanhoCamisa}</td>
+              <td className="border border-gray-700 p-0.5 text-center font-semibold w-[130px]">PROFESSOR ORIENTADOR</td>
+              <td className="border border-gray-700 p-0.5">{professor.nome}</td>
+              <td className="border border-gray-700 p-0.5">{professor.email}</td>
+              <td className="border border-gray-700 p-0.5">{maskCPF(professor.cpf)}</td>
+              <td className="border border-gray-700 p-0.5 text-center">{professor.dataNascimento && formatDate(professor.dataNascimento.toString())}</td>
+              <td className="border border-gray-700 p-0.5 text-center">{professor.tamanhoCamisa}</td>
             </tr>
           ))}
         </tbody>
@@ -125,28 +125,28 @@ export const TeamRegisterPrintComponent = ({ id }: TeamRegisterPrintComponentPro
       <table className="w-full border border-t-0 border-gray-700 text-sm">
         <tbody>
           <tr>
-            <td className="border border-gray-700 font-semibold border-t-0 align-top w-[130px] p-1">
+            <td className="border border-gray-700 font-semibold border-t-0 align-top w-[130px] p-0.5">
               Provável(eis) Objetivo(s) de Desenvolvimento Sustentável(eis)
             </td>
-            <td className="border border-t-0 border-gray-700 p-1">
+            <td className="border border-t-0 border-gray-700 p-0.5">
               {teamRegister?.odsList?.map((ods) => ods.descricao).join(". ")}
             </td>
           </tr>
 
           <tr>
-            <td className="border border-gray-700 font-semibold align-top w-[130px] p-1">
+            <td className="border border-gray-700 font-semibold align-top w-[130px] p-0.5">
               Provável(eis) Atividade(s) Empreendedora(s)
             </td>
-            <td className="border border-gray-700 p-1">
+            <td className="border border-gray-700 p-0.5">
               {teamRegister?.tipoAtividades?.map((atv) => atv.descricao).join(". ")}
             </td>
           </tr>
 
           <tr>
-            <td className="border border-gray-700 font-semibold align-top w-[130px] p-1">
+            <td className="border border-gray-700 font-semibold align-top w-[130px] p-0.5">
               Instituição de Impacto Social parceira do Time
             </td>
-            <td className="border border-gray-700 p-1">
+            <td className="border border-gray-700 p-0.5">
               {teamRegister?.instituicoes?.map((inst) => inst.descricao).join(". ")}
             </td>
           </tr>
@@ -155,7 +155,7 @@ export const TeamRegisterPrintComponent = ({ id }: TeamRegisterPrintComponentPro
 
 
 
-      <div className="border border-gray-700 border-t-0 p-1">
+      <div className="border border-gray-700 border-t-0 p-0.5">
         <div className="border-gray-700 text-sm">
           <div className="flex justify-between mb-4">
             <p>Para Uso da Tesouraria</p>

091b6a768eff020d859a2befeb841477c7e6a30d
winnicius-moura <winnicius.moura@gmail.com>
2025-09-14 18:32:52 -0300

    feat: add team selection option for professors in logout component

--- Frontend/plataformaempreendedorismo/src/admin/components/common/logout.tsx
+++ Frontend/plataformaempreendedorismo/src/admin/components/common/logout.tsx
@@ -1,3 +1,4 @@
+import GroupIcon from '@mui/icons-material/Group'
 import LogoutIcon from '@mui/icons-material/Logout'
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
@@ -5,6 +6,7 @@ import { RoutesNames } from "../../../globals"
 import { logout } from '../../../redux/reducers/auth.slice'
 import { clearUserInfo } from '../../../redux/reducers/userInfo.slice'
 import { persistor, RootState } from '../../../redux/store'
+import { Roles } from '../../../utils/types'
 import { AuthMenuComponent } from './authMenu'
 
 export const Logout = () => {
@@ -12,11 +14,32 @@ export const Logout = () => {
   const navigate = useNavigate();
   const userGlobalState = useSelector((state: RootState) => state.userInfo);
 
+  const shouldShowTeamSelection = userGlobalState?.enumRole === Roles.Professor &&
+    userGlobalState?.idEquipe &&
+    userGlobalState.idEquipe.length > 1
+
+  const handleTeamSelectionClick = () => {
+    navigate(RoutesNames.teamSelection)
+  }
+
   return (
-    <div className='flex items-center'>
+    <div className='flex items-center gap-1'>
+      {shouldShowTeamSelection && (
+        <div
+          className="flex gap-1 cursor-pointer text-ring-custom "
+          onClick={handleTeamSelectionClick}
+          title="Lista de Seleção de Equipes"
+        >
+          <GroupIcon className="text-right" />
+          <button className="text-right">Equipes</button>
+        </div>
+      )}
+
       {userGlobalState?.email ? (
         <AuthMenuComponent email={userGlobalState.email} />
       ) : null}
+
+      {/* Botão de Logout */}
       <div
         className="flex gap-1 items-center cursor-pointer text-ring-custom hover:text-red-400"
         onClick={() => {

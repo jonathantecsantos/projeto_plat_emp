4f8035851551f7016fbb85c9e71fae83db3880f8
Jonathan Santos <jonathan.tecsantos@gmail.com>
2025-10-12 12:28:45 -0300

    Aumentando tamanho de upload de arquivos

--- Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/infra/config/ExceptionHandlerAdvice.java
+++ Backend/plataformaempreendedorismo/src/main/java/com/plataforma/empreendedorismo/plataformaempreendedorismo/infra/config/ExceptionHandlerAdvice.java
@@ -1,9 +1,11 @@
 package com.plataforma.empreendedorismo.plataformaempreendedorismo.infra.config;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import util.exceptions.EventoEncontradoException;
 import util.exceptions.EventoNaoEncontradoException;
 
@@ -22,5 +24,17 @@ public class ExceptionHandlerAdvice {
     @ResponseStatus(HttpStatus.CONFLICT)
     public Map<String, String> handleEventoEncontrado(EventoEncontradoException ex) {
         return Map.of("message", ex.getMessage());
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException ex) {
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body("A soma do tamanho dos arquivos não pode exceder 30MB.");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Erro inesperado: " + ex.getMessage());
     }
 }
